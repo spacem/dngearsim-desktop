aa68f27be8ee7f9cae19bda5291d0aa23fdc9ccd
spacem <spacem@github.com>
2019-08-08 20:01:25 +1000

    support for processing of all-items

--- gear-sim-window/actions/get-file-action.js
+++ gear-sim-window/actions/get-file-action.js
@@ -19,6 +19,8 @@ module.exports = class GetFileAction {
                 result = dnTranslations.process(buffer.toString());
             });
             return result;
+        } else if(this.fileName === 'all-items.json') {
+            return this.processAllItems();
         } else if(this.fileName.indexOf('.json') > 0) {
             const fileName = this.fileName.replace('.optimised', '');
             const dntReader = new DntReader();
@@ -28,6 +30,46 @@ module.exports = class GetFileAction {
             return dntReader;
         }
         return 'some file called ' + this.fileName + ' from ' + this.folder;
+    }
+
+    async processAllItems() {
+        const itemDntReader = new DntReader();
+        itemDntReader.columnNames = [
+            'id',
+            'NameID',
+            'NameIDParam',
+            'Rank',
+            'IconImageIndex',
+            'Type',
+            'LevelLimit',
+            'fileName'
+        ];
+        itemDntReader.numColumns = itemDntReader.columnNames.length;
+        itemDntReader.data = [];
+        await GetFileAction.pakUtil.processFiles('itemtable', async (fileName, buffer) => {
+            const dntReader = new DntReader();
+            dntReader.processFile(buffer, fileName);
+            if(dntReader.numRows > 0 && 
+                'NameID' in dntReader.columnIndexes && 
+                'Type' in dntReader.columnIndexes && 
+                'IconImageIndex' in dntReader.columnIndexes) {
+            
+                  for(var i=0;i<dntReader.numRows;++i) {
+                    var id = dntReader.getValue(i, 'id');
+                    itemDntReader.data.push([
+                      id,
+                      dntReader.getValue(i, 'NameID'),
+                      dntReader.getValue(i, 'NameIDParam'),
+                      dntReader.getValue(i, 'Rank'),
+                      dntReader.getValue(i, 'IconImageIndex'),
+                      dntReader.getValue(i, 'Type'),
+                      dntReader.getValue(i, 'LevelLimit'),
+                      fileName
+                    ]);
+                  }
+            }
+        });
+        return itemDntReader;
     }
 
     setupPaks(folder) {
